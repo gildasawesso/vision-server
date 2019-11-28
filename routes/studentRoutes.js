c9f2917b9932d7594e570ed4937b069a8ea1a76d
gildas awesso <gildas.dev@gmail.com>
2019-11-28 15:20:28 +0100

    fixed update on students

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -4,7 +4,7 @@ const studentController = require('../controllers/studentController');
 
 router.get('/', studentController.get);
 router.post('/', studentController.add);
-router.put('/:id', studentController.update);
+router.patch('/:id', studentController.update);
 router.delete('/:id', studentController.delete);
 
 module.exports = router;
